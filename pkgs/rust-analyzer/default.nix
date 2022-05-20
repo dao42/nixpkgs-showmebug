9039538c6c5a16ec69e3fcf2fd9a4a843ff1e13a
hongcha <1448983340@qq.com>
2022-05-20 14:01:21 +0800

    lsp rust

---
+++ pkgs/rust-analyzer/default.nix
@@ -0,0 +1,19 @@
+{ stdenv, fetchurl }:
+stdenv.mkDerivation rec {
+  pname = "rust-analyzer";
+  version = "2022-05-17";
+  
+  src = fetchurl {
+    url = "http://119.91.141.92:8080/language-source/rust/rust-analyzer";
+    sha256 = "sha256-W7zkpNT1ca0SqIrqnhW+wm1kroM3+sI/Sos93m+eDcA=";
+  };
+
+  phases = [ "installPhase" ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${src} $out/bin
+    mv $out/bin/*-rust-* $out/bin/rust-analyzer
+    cd $out/bin &&  chmod +x rust-analyzer
+  '';
+}
