de8a1f2999aa9e423b092f358885e039d322750c
hongcha <zhangsifan98@gmail.com>
2022-09-19 15:50:43 +0800

    feat(pkg): add cppdap

---
+++ pkgs/cppdap/default.nix
@@ -0,0 +1,19 @@
+{ stdenv,fetchurl }:
+stdenv.mkDerivation rec {
+  pname = "cppdap";
+  version = "2022091901";
+  
+  src = fetchurl {
+    url = "http://119.91.141.92:8080/debug/cppdap-2022091901.tar.gz";
+    sha256 = "sha256:1h0yydqy5602sxb5wjwj0fkbf5jb9lavj7f24r3yf7hiipwjiggw";
+  };
+
+
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r ./bin/* $out/bin
+  '';
+
+  dontStrip = true;
+}
